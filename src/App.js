bf5e447c7a1aa997543f9bf34a86d395e70a45a4
in-key <inkeydev@gmail.com>
2023-05-08 20:10:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e.target.state.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pharmacist CE Requirement Checker</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>State: </label>
+        <select name="state" onChange={(e) => console.log(e.target.value)}>
+          <option value="default">Select Your State</option>
+          <option value="NJ">NJ</option>
+          <option value="NY">NY</option>
+        </select>
+        <input type="submit"></input>
+      </form>
+      <table>
+        <tr>
+          <th>Credit Type</th>
+          <th>Credit Number</th>
+          <th>Requirement Met?</th>
+        </tr>
+        <tr>
+          <th>Live</th>
+          <th>10</th>
+          <th>No</th>
+        </tr>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
